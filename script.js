e0fe1efbb45b1df7e2b5fbae20834f326346c344
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-03-18 14:20:28 -0400

    deleted instruction comments

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Obtain the img element, and assign it to the movie variable 
 let movie = document.querySelector('#movie')
 
-// Obtain the 'change-border' button, and assign it to the changeBorder variable
 let changeBorder = document.querySelector('#change-border')
 
 //DO NOT CHANGE THE FOLLOWING CODE
